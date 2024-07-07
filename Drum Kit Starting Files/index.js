60268719e8f889c975691cfad2aeb5cec0fa1db5
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-07-07 15:52:18 -0500

    Drum kit website is completed

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,21 +1,36 @@
-var btns = document.querySelectorAll(".drum");
 
-for(var i = 0; i < btns.length; i++){
-    btns[i].addEventListener("click", function (){
-        switch(this.innerText){
-            case "w" : {playAudio("sounds/tom-1.mp3"); break;}
-            case "a" : {playAudio("sounds/tom-2.mp3"); break;}
-            case "s" : {playAudio("sounds/tom-3.mp3"); break;}
-            case "d" : {playAudio("sounds/tom-4.mp3"); break;}
-            case "j" : {playAudio("sounds/snare.mp3"); break;}
-            case "k" : {playAudio("sounds/kick-bass.mp3"); break;}
-            case "l" : {playAudio("sounds/crash.mp3"); break;}
-        }
+
+// Detecting button click
+var btns = document.querySelectorAll(".drum");
+for (let i = 0; i < btns.length; i++) { // Use `let` instead of `var`
+    btns[i].addEventListener("click", function() {
+        var btnInnerHTML = btns[i].innerHTML; // Correctly scoped to each button
+        makeSound(btnInnerHTML);
+        alert(i);
+        buttonAnimation(btnInnerHTML);
     });
 }
 
-function handleClick(){
-    alert("I got clicked");
+
+
+// Detecting keyboard press
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+
+function makeSound(key){
+    switch(key){
+        case "w" : {playAudio("sounds/tom-1.mp3"); break;}
+        case "a" : {playAudio("sounds/tom-2.mp3"); break;}
+        case "s" : {playAudio("sounds/tom-3.mp3"); break;}
+        case "d" : {playAudio("sounds/tom-4.mp3"); break;}
+        case "j" : {playAudio("sounds/snare.mp3"); break;}
+        case "k" : {playAudio("sounds/kick-bass.mp3"); break;}
+        case "l" : {playAudio("sounds/crash.mp3"); break;}
+    }
 }
 
 function playAudio(filePath){
@@ -23,3 +38,10 @@ function playAudio(filePath){
     audio.play();
 }
 
+function buttonAnimation(currentKey){
+    var activeBtn = document.querySelector("." + currentKey);
+    activeBtn.classList.add("pressed");
+    setTimeout(function (){
+        activeBtn.classList.remove("pressed");
+    }, 1000);
+}
