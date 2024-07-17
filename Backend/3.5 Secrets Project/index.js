aaff46d60302b4f74e7a8b0bc1ea10261fc6f4b3
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-07-17 14:31:28 -0500

    3.5 Secrets Project completed

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,45 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+function passwordCheck(req, res, next){
+    let password = req.body["password"];
+    if (password === "ILoveProgramming") {
+        userIsAuthorised = true;
+      }
+    next();
+}
+
+app.use(passwordCheck);
+
+app.post("/check", (req, res) => {
+    console.log("Inside the post: Authorized = " + userIsAuthorised);
+    if (userIsAuthorised) {
+        res.sendFile(__dirname + "/public/secret.html");
+      } else {
+        res.sendFile(__dirname + "/public/index.html");
+        //Alternatively res.redirect("/");
+      }
+})
+
+app.get("/", (req,res) =>{
+    console.log("Inside the get: Authorized = " + userIsAuthorised);
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(3000, () => {
+    console.log(`Server on port: ${port} has started`);
+});
