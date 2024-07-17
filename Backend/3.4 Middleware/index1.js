75f09d0e173f6834748e378f11da6fdf0657e055
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-07-17 00:48:24 -0500

    3.4 Into to Middlewares completed

--- Backend/3.4 Middleware/index1.js
+++ Backend/3.4 Middleware/index1.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json({extended : true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
